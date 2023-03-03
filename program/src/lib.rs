b4d0024de83e610fe5e4fa43b23fb0aee40b4dfc
owlman2222 <owlman2222@yahoo.co.uk>
2023-03-03 20:24:18 +0000

    update

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -19,4 +19,4 @@ pub use safecoin_program;
 /// Max name length for any of the names used in this crate.
 pub const MAX_NAME_LENGTH: usize = 32;
 
-safecoin_program::declare_id!("auth9SigNpDKz4sJJ1DfCTuZrZNSAgh9sFD3rboVmgg");
+safecoin_program::declare_id!("authvoCCz9WxYk6ZQH1smw5xFT9zRGJa1QoAHBZ2HBy");
